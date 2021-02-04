7359dd82c889b652a0096e31a53fc860bf23cc99
tomas <72792707+tomas-land@users.noreply.github.com>
2021-02-04 13:41:09 +0200

    commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# FsBrowserPHP
+
+## About
+This is a file system browser write in php, used as a practical task in PHP programming course. The app:
+- allows users to see the contents of the folder starting from the folder the app is installed in
+- delete / upload / download files
+- demonstartes simple login / logout flow
+
+## Installation
+- Clone the repostory
+- Open the path where you can launch php interpreter, e.g.: http//localhost/FsBrowserPHP
+- Login w/ credentials that are provided in the form as placeholders.
+
+## Usage
+- The functionality should be intuitive
+- Important: the app illustrates how to develop in a gradual manner, each git branch illustrates a different step in the process.
+- The most complete app / functionality is in the task-1.4 branch 
--- index.php
+++ index.php
@@ -1,124 +1,153 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>File System Browser</title>
 </head>
+<style>
+    * {
+        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    }
+
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    table td,
+    table th {
+        border: 1px solid #ddd;
+        padding: 8px;
+    }
+
+    table tr:nth-child(even) {
+        background-color: #f2f2f2;
+    }
+
+    table tr:hover {
+        background-color: #ddd;
+    }
+
+    table th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #4CAF50;
+        color: white;
+    }
+    .create_back {
+        
+        display: flex;
+        justify-content: space-between;
+        margin: 50px;
+
+    }
+</style>
 
 <body>
-
-<!-- LOGIN--------------------------------- -->
-<?php
-session_start();
-$username="123";
-$password="123";
-
-if($_POST['username'] == $username && $_POST['password'] == $password){
-    header( 'Location:index.php' );
-}
-else
-{
-?>
-
-        <form  action="index.php" method='POST' >
-            <label for='username' >UserName:</label>
-            <input type='text' placeholder="123" name='username'  maxlength="50" />
-            <label for='password' >Password:</label>
-            <input type='text' name='password' placeholder="123" maxlength="50" />
-            <input type='submit' name='submit' value='Submit' />
-           
-        </form>
-
-<?php
-
-}
-?>
-
-
-
-<!-- CREATE FOLDER------------------------------ -->
     <?php
-    
-    $folder_name = $_POST['createfolder'];
-    if (!file_exists($path . $folder_name)) {
-        // $current_path = getcwd();
-        @mkdir($folder_name, true);
-    }
-    ?>
 
-    <form action="" method="post">
-        <h2>
-            Create New Folder
-        </h2>
-        <input name="createfolder" type="text">
-        <input type="submit" value="Create Folder">
-    </form>
-    <br>
+    // $path = './' . $_GET['path'];
+    // $files_and_dirs = scandir($path);
 
-<!-- MAIN------------------------ -->
+    // print('<h2>Directory contents: ' . str_replace('?path=','',$_SERVER['REQUEST_URI']) . '</h2>');
 
-    <?php
-    $path = isset($_GET['path']) ? $_GET['path'] : './';
-    $files = scandir($path);
-    print('<table>
-        <tr>
-            <th>Type</th>
-            <th>Name</th>
-            <th>Action</th>
-        </tr>');
+    // // List all files and directories
+    // print('<table><th>Type</th><th>Name</th><th>Actions</th>');
+    // foreach ($files_and_dirs as $fnd){
+    //     if ($fnd != ".." and $fnd != ".") {
+    //         print('<tr>');
+    //         print('<td>' . (is_dir($fnd) ? "Directory" : "File") . '</td>');
+    //         print('<td>' . (is_dir($fnd) 
+    //                     ? '<a href="' . (isset($_GET['path']) 
+    //                             ? $_SERVER['REQUEST_URI'] . $fnd . '/' 
+    //                             : $_SERVER['REQUEST_URI'] . '?path=' . $fnd . '/') . '">' . $fnd . '</a>'
+    //                     : $fnd) 
+    //             . '</td>');
+    //         print('<td></td>');
+    //         print('</tr>');
+    //     }
+    // }
+    // print("</table>");
+    $path = './' . $_GET['path'];
 
-    foreach ($files as $file) {
-        if ($file != '.' && $file != '..') {
 
-            print('<tr><td>');
-            if (is_dir($file)) {
-                echo 'directory';
+    function display($files_and_dirs)
+    {
+        $path = './' . $_GET['path'];
+        $files_and_dirs = scandir($path);
+
+        print('<table><th>Type</th><th>Name</th><th>Actions</th>');
+        foreach ($files_and_dirs as $fnd) {
+            if ($fnd != ".." and $fnd != ".") {
+                print('<tr>');
+                print('<td>' . (is_dir($fnd) ? "Directory" : "File") . '</td>');
+                print('<td>' .
+                    '<a href="' . (is_dir($fnd) ? ((isset($_GET['path'])) ? $_SERVER['REQUEST_URI'] . $fnd . '/' : $_SERVER['REQUEST_URI'] . '?path=' . $fnd . '/') :  $fnd) . '">' . $fnd . '</a>');
+                print('</td></tr>');
+                
             }
-            print('</td>');
-            
-            print('<td>' .
-                '<a href=?path=' . $path . $file . '>' . $file . '</a>' .
-                '</td><td>
-                <form action="" method="POST">
-                <input type="hidden" name="delete" value="' . $file . '">
-                <button class="del_btn" type="submit">Delete</button>
-            </form>
-        
-        </td></tr>');
+        }
+        print("</table>");
+    }
+
+
+    //create folder
+    if (isset($_POST['create'])) {
+        $new_folder = $path . $_POST['create'];
+        if (is_dir($new_folder)) {
+            echo 'Directory exist';
+        } else {
+            mkdir($new_folder);
         }
     }
 
-    print('</table>');
-    
-    // DELETE BUTTON-------------------
-
-    if (isset($_POST['delete'])) {
-        $delete_file = $_GET['path'] . "/" . $_POST['delete'];
-                unlink($delete_file);
-                
-            }
+    // function back($url){
+    // $url = htmlspecialchars($_SERVER['HTTP_REFERER']);
+    // echo "<a href='$url'>back</a>"; 
+    // }
     
 
-
-
-    // BACK BUTTON--------------------
+  function back_url($url_back_2){
+        $url_back_2 = dirname($_SERVER['REQUEST_URI']);
+        print("<a href='$url_back_2'>GO BACK</a>");
+  }
     
-    function back_button($url) {
-        $url = dirname($_SERVER['REQUEST_URI']);
-        print("<div><button><a href='$url'>BACK</a></button></div>");
-    }
-    back_button($url_back_2);
+
+
+  print_r($_POST).'<br>';
+    print_r((__FILE__).'<br>');
+    print_r(dirname(__FILE__).'<br>');
+    print_r($_SERVER['REQUEST_URI'].'<br>');
+    print(dirname($_SERVER['REQUEST_URI']).'<br>');
+
+    
     ?>
 
 
+  
+
+
+
+    <?php
+    display($files_and_dirs);
+
+
+    ?>
+
+<!-- -------------HTML -->
+<br>
+   <div class="create_back">
+   
+    <form action="" method="POST">
+        <input type="text" name="create">
+        <input type="submit" value="Create directory">
+
+    </form>
 
     
-
-
-
+    <button><?php echo back_url($url_back_2)?></button>
+</div>
 
 </body>
 
